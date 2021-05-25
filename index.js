9db47cb60acc98086fdfde40bd4b12daf93a3f3d
Magno Milhomem <magno.milhomem@gmail.com>
2021-05-25 13:46:15 -0300

    first commit

---
+++ index.js
@@ -0,0 +1,11 @@
+import { Cliente } from "./Cliente.js"
+import { ContaCorrente } from "./ContaCorrente.js"
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+
+const contaCorrenteRicardo = new ContaCorrente(1001, cliente1);
+contaCorrenteRicardo.depositar(500);
+contaCorrenteRicardo.sacar(100);
+const contaPoupanca = new contaPoupanca();
+
+console.log(contaCorrenteRicardo);
